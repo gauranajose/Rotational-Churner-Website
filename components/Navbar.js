12261a9f30a03b08f6b32cd7a2cd33fe61bf760c
Jose <gauranajose@gmail.com>
2021-07-26 19:29:43 +0800

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => (
+  <div className="border-b border-gray-300 flex justify-center w-full fixed bg-white">
+    <div className="w-100 h-20 flex justify-between items-center">
+      <p className="text-3xl font-bold tracking-tighter text-blue-600">Rotational Churner</p>
+    </div>
+  </div>
+);
+
+export default Navbar
